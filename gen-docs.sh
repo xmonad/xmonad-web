85dc28bfc3632c9d97d1494b0cdc2516a6498088
Peter Jones <pjones@devalot.com>
2017-02-10 18:02:25 -0700

    Get gen-docs.sh working, update docs

--- gen-docs.sh
+++ gen-docs.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/sh -eu
 
+release="0.13"
 startdir=`pwd`
 
 mkdir -p xmonad-docs
@@ -8,34 +9,58 @@ destdir=`pwd`
 
 tmp=`mktemp -d`
 
+cleanup () {
+  rm -rf $tmp
+}
+
+trap "cleanup" EXIT
+
 gendocs () {
     cd $tmp
-    if [ -e "$startdir/$1-$2.tar.gz" ]; then cp $startdir/$1-$2* .; tar xf $1-$2*;
-            else cabal unpack $1;
+
+    name=$1
+    flags=""
+    ver=""
+
+    if [ $# -gt 1 ]; then
+      ver=$2
     fi
-    cd $1*
-    if [ -n "$3" ]; then flags="--flags='$3'";
+
+    if [ $# -gt 2 ]; then
+      flags="--flags='$3'";
     fi
-    runhaskell Setup configure --user $flags
-    runhaskell  Setup haddock --hyperlink-source --html-location='http://xmonad.org/xmonad-docs/$pkg'
+
+    if [ -e "$startdir/$name-$ver.tar.gz" ]; then
+      cp $startdir/$name-$ver* .; tar xf $name-$ver*;
+    else
+      cabal unpack $name;
+    fi
+
+    cd $name*
+
+    cabal sandbox init
+    cabal install --only-dependencies
+    cabal configure $flags
+    cabal haddock --hyperlink-source --html-location='http://xmonad.org/xmonad-docs/$pkg'
+
     cp dist/doc/html/*/*.haddock $destdir
     rsync -a dist/doc/html/ $destdir
 }
 
-gendocs xmonad 0.11
+gendocs xmonad $release
 gendocs X11
 gendocs X11-xft
-# gendocs utf8-string
-gendocs xmonad-contrib 0.11 "with_xft"
-
-rm -r $tmp
+gendocs xmonad-contrib $release "with_xft"
 
 cd $destdir
+haddock_args=""
+
 for name in */; do
     for x in ${name}*.haddock; do
         n=`echo $name | tr -d '/'`
         haddock_args="$haddock_args --read-interface=$n,$x"
     done
 done
+
 echo $haddock_args
 haddock -t 'Welcome to XMonad' --gen-contents --gen-index -o . --prologue $startdir/prologue.txt $haddock_args
