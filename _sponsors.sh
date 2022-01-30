4dca25de72ee84ecd835b91a59d99d730ec38311
Tomas Janousek <tomi@nomi.cz>
2022-01-30 23:41:07 +0000

    _sponsors: Include $25+ custom tiers in named sponsors
    (The sponsorshipsAsMaintainer API wasn't there when this was first
    written.)

--- _sponsors.sh
+++ _sponsors.sh
@@ -5,95 +5,87 @@ shopt -s lastpipe inherit_errexit
 
 function o { printf -->&2 "%s:%s\\n" "${0##*/}" "$(printf " %q" "$@")"; "$@"; }
 
-function gh-org-sponsors-tiers {
+function gh-org-sponsors-monthly-tiers {
 	# shellcheck disable=SC2016
-	gh api graphql -F org="${1:?org}" -f query='
-		query Tiers($org: String!) {
+	gh api graphql --paginate -F org="${1:?org}" -f query='
+		query Tiers($endCursor: String, $org: String!) {
 			organization(login: $org) {
 				... on Sponsorable {
 					sponsorsListing {
-						tiers(first: 100) {
+						tiers(first: 100, after: $endCursor) {
 							nodes {
 								id
 								name
 								description
+								isOneTime
 								monthlyPriceInDollars
 							}
+							pageInfo { hasNextPage, endCursor }
 						}
 					}
 				}
 			}
 		}
-	' --jq '.data.organization.sponsorsListing.tiers.nodes[]'
+	' --jq '.data.organization.sponsorsListing.tiers.nodes[] | select(.isOneTime | not)'
 }
 
-function gh-org-sponsors-at-tier {
-	# shellcheck disable=SC2016
-	gh api graphql --paginate -F org="${1:?org}" -F tier="${2:?tier}" -f query='
-		query Sponsors($endCursor: String, $org: String!, $tier: ID!) {
-			organization(login: $org) {
-				... on Sponsorable {
-					sponsors(first: 100, tierId: $tier, after: $endCursor) {
-						nodes {
-							... on User         { login, name, url, websiteUrl }
-							... on Organization { login, name, url, websiteUrl }
-						}
-						pageInfo {
-							hasNextPage
-							endCursor
-						}
-					}
-				}
-			}
-		}
-	' --jq '.data.organization.sponsors.nodes[]'
-}
-
-function gh-org-sponsors {
+function gh-org-sponsorships {
 	# shellcheck disable=SC2016
 	gh api graphql --paginate -F org="${1:?org}" -f query='
 		query Sponsors($endCursor: String, $org: String!) {
 			organization(login: $org) {
 				... on Sponsorable {
-					sponsors(first: 100, after: $endCursor) {
+					sponsorshipsAsMaintainer(first: 100, includePrivate: true, after: $endCursor) {
 						nodes {
-							... on User         { login, name, url, websiteUrl }
-							... on Organization { login, name, url, websiteUrl }
+							sponsorEntity {
+								... on User         { login, name, url, websiteUrl }
+								... on Organization { login, name, url, websiteUrl }
+							}
+							tier {
+								closestLesserValueTier { id }
+							}
+							privacyLevel
 						}
-						pageInfo {
-							hasNextPage
-							endCursor
-						}
+						pageInfo { hasNextPage, endCursor }
 					}
 				}
 			}
 		}
-	' --jq '.data.organization.sponsors.nodes[]'
+	' --jq '.data.organization.sponsorshipsAsMaintainer.nodes[]'
 }
 
 function jq-tierids-since {
 	jq --arg since "${1:?since}" -s -r 'until(.[0].description | contains($since); del(.[0])) | .[].id'
 }
 
+function jq-filter-privacylevel {
+	jq --arg level "${1:?level}" 'select(.privacyLevel == $level)'
+}
+
+function jq-filter-tier {
+	jq --arg tier "${1:?tier}" 'select(.tier.closestLesserValueTier.id == $tier)'
+}
+
 function jq-sort-by {
 	jq -s "sort_by(.${1:?key}) | .[]"
 }
 
-function jq-intersect-by {
-	jq --argjson filter "${2:?filter}" "select([.${1:?key}] | inside([\$filter[].${1:?key}]))"
-}
-
 function named-sponsors {
-	public_sponsors=$(o gh-org-sponsors "${1:?org}" | jq -s '')
-	GITHUB_TOKEN="${ADMIN_GITHUB_TOKEN:?}" o gh-org-sponsors-tiers "${1:?org}" | \
-		jq-tierids-since XMonad.Layout.Named | \
-		tac | \
-		while read -r tier; do
-			GITHUB_TOKEN="${ADMIN_GITHUB_TOKEN:?}" o gh-org-sponsors-at-tier "${1:?org}" "$tier" \
-				| jq-sort-by login
-		done | \
-		jq-intersect-by login "$public_sponsors" | \
-		jq -s ''
+	public_sponsorships=$(
+		GITHUB_TOKEN="${ADMIN_GITHUB_TOKEN:?}" \
+		o gh-org-sponsorships "${1:?org}" \
+		| jq-filter-privacylevel PUBLIC \
+		| jq-sort-by sponsorEntity.login \
+		| jq -s ''
+	)
+	GITHUB_TOKEN="${ADMIN_GITHUB_TOKEN:?}" \
+	o gh-org-sponsors-monthly-tiers "${1:?org}" \
+	| jq-tierids-since XMonad.Layout.Named \
+	| tac \
+	| while read -r tier; do
+		<<<"$public_sponsorships" jq '.[]' | jq-filter-tier "$tier" | jq '.sponsorEntity'
+	done \
+	| jq -s ''
 }
 
 "$@"
